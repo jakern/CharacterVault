c976366d28b0262624075f2d05aba8870ec1bd4e
Jacob Kern <wand3r0r@gmail.com>
2017-04-02 13:22:10 -0400

    init commit

---
+++ app/models/npc.js
@@ -0,0 +1,81 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var NPCSchema   = new Schema({
+		
+		title:String,
+		exp: Number,
+		size: String,
+		footprint: {
+			x: Number,
+			y: Number,
+			z: Number
+		},
+		reach: Number,
+		type: [String],
+		mobility: {
+			primaryMotionType: String,
+			groundSpeed: Number,
+			burrowSpeed: Number,
+			flySpeed: Number,
+			swimSpeed: Number
+		},
+		templates: {
+			rogue: [String],
+			monster: [String]
+		},
+		attributes: {
+			strength: Number,
+			dexterity: Number,
+			constitution: Number,
+			intelligence: Number,
+			wisdom: Number,
+			charisma: Number
+		},
+		traits: {
+			initative: Number,
+			attack: Number,
+			defense: Number,
+			resilience: Number,
+			health: Number,
+			competence: Number
+		},
+		signatureSkills: [
+		{
+			name: String,
+			level: Number
+		}
+		],
+		spellcasting: {
+			skilGrade: Number,
+			spells: [String]
+		},
+		qualities: [
+		{
+			name: String,
+			notes: String
+		}
+		],
+		treasure: {
+			any: Number,
+			coin: Number,
+			gear: Number,
+			loot: Number,
+			magic: Number,
+			trophies: Number
+		},
+		gear: {
+			mountsAndVehicles: String,
+			gear: String
+		},
+		attacks: [
+		{
+			attackType: String,
+			name: String,
+			details: String
+		}
+		]
+		
+});
+
+module.exports = mongoose.model('NPC', NPCSchema);
---
+++ server.js
@@ -0,0 +1,119 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var morgan     = require('morgan');
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 8080; // set our port
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://192.168.1.21:27072'); // connect to our database
+var NPC     = require('./app/models/npc');
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'Welcome to CharacterVault. ' });
+});
+
+// on routes that end in /npcs
+// ----------------------------------------------------
+router.route('/npcs')
+
+	// create a npc (accessed at POST http://localhost:8080/npcs)
+	.post(function(req, res) {
+
+		var npc = new NPC(req.query);		// create a new instance of the NPC model
+		npc.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: "Character Created!" });
+		});
+
+
+	})
+
+	// get all the npcs (accessed at GET http://localhost:8080/api/npcs)
+	.get(function(req, res) {
+		NPC.find(function(err, npcs) {
+			if (err)
+				res.send(err);
+
+			res.json(npcs);
+		});
+	});
+
+// on routes that end in /npcs/:npc_id
+// ----------------------------------------------------
+router.route('/npcs/:npc_id')
+
+	// get the npc with that id
+	.get(function(req, res) {
+		NPC.findById(req.params.npc_id, function(err, npc) {
+			if (err)
+				res.send(err);
+			res.json(npc);
+		});
+	})
+
+	// update the npc with this id
+	.put(function(req, res) {
+		NPC.findById(req.params.npc_id, function(err, npc) {
+
+			if (err)
+				res.send(err);
+
+			npc.name = req.body.name;
+			npc.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'NPC updated!' });
+			});
+
+		});
+	})
+
+	// delete the npc with this id
+	.delete(function(req, res) {
+		NPC.remove({
+			_id: req.params.npc_id
+		}, function(err, npc) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
